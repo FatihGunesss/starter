0b5dc14a1404a59ead2b169fc5993ee4e2a7503e
FATİH GÜNEŞ <icefatih13@gmail.com>
2024-04-22 15:25:14 +0500

    fıle structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Footer, Header } from 'components';
+import { Home } from 'pages';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/confing.js
@@ -0,0 +1,9 @@
+    const config = {
+    APP_NAME: "Coffee-Roaster",
+    STATIC_FILES_DIR: process.env.REACT_APP_ROOT_STATIC_FILES_DIR || "",
+  };
+  
+  
+  
+  export default config;
+  
